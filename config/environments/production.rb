cd5ea97ea8f10225c97963312a8778d18ac78241
Laetitia Massa <laetitiamassa@gmail.com>
2016-01-10 13:02:40 +0100

    added mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,23 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'start.hackuna.org' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+
+  #for email notifications
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587,
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => ENV['MANDRILL_USERNAME'],
+    :password  => ENV['MANDRILL_API_KEY'], # SMTP password is any valid API key
+    :authentication => 'login',
+    :domain => 'start.hackuna.org'
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
